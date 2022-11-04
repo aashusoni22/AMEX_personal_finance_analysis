12eee2c920e0eec06c877768f24f948124ba6e8d
Aashutoshkumar Soni <114427519+aashusoni22@users.noreply.github.com>
2022-11-03 21:24:38 -0400

    Update AMEX_personal_finance_analysis.sql

--- AMEX_personal_finance_analysis.sql
+++ AMEX_personal_finance_analysis.sql
@@ -16,14 +16,14 @@ ORDER BY total_spend DESC
 
 WITH online_purchase AS(
 SELECT MONTH(date) AS month, 
-		SUM(CASE WHEN YEAR(date) = 2021 THEN amount END) AS total_spent_2021, 
-		SUM(CASE WHEN YEAR(date) = 2022 THEN amount END) AS total_spent_2022
+	SUM(CASE WHEN YEAR(date) = 2021 THEN amount END) AS total_spent_2021, 
+	SUM(CASE WHEN YEAR(date) = 2022 THEN amount END) AS total_spent_2022
 FROM AMEX..Amex
 WHERE description LIKE 'AMZN% ' OR upper(description) LIKE 'AMAZON%' OR upper(description) LIKE 'CHILDERNS%' OR upper(description) LIKE 'ARDEN%'
-					OR upper(description) LIKE 'FEDEX%' OR upper(description) LIKE 'UDEMY%' OR upper(description) LIKE 'NIKE%' 
-					OR upper(description) LIKE 'IMMIGRATION%' OR upper(description) LIKE 'CV%' OR upper(description) LIKE 'EBAY%'
-					OR upper(description) LIKE 'PAYTM%' OR upper(description) LIKE 'OEIS%' OR upper(description) LIKE 'KINDLE%'
-					OR upper(description) LIKE 'ONTARIO%'
+	OR upper(description) LIKE 'FEDEX%' OR upper(description) LIKE 'UDEMY%' OR upper(description) LIKE 'NIKE%' 
+	OR upper(description) LIKE 'IMMIGRATION%' OR upper(description) LIKE 'CV%' OR upper(description) LIKE 'EBAY%'
+	OR upper(description) LIKE 'PAYTM%' OR upper(description) LIKE 'OEIS%' OR upper(description) LIKE 'KINDLE%'
+	OR upper(description) LIKE 'ONTARIO%'
 GROUP BY MONTH(date)
 )
 SELECT month, total_spent_2021,total_spent_2022, ROUND(100*(total_spent_2022-total_spent_2021)/total_spent_2021,2) as percent_change
@@ -34,12 +34,12 @@ FROM online_purchase
 
 WITH instore_purchase AS(
 SELECT MONTH(date) AS month, 
-		SUM(CASE WHEN YEAR(date) = 2021 THEN amount END) AS total_spent_2021, 
-		SUM(CASE WHEN YEAR(date) = 2022 THEN amount END) AS total_spent_2022
+	SUM(CASE WHEN YEAR(date) = 2021 THEN amount END) AS total_spent_2021, 
+	SUM(CASE WHEN YEAR(date) = 2022 THEN amount END) AS total_spent_2022
 FROM AMEX..Amex
 WHERE upper(description) LIKE 'ESSO% ' OR upper(description) LIKE 'MACS%' OR upper(description) LIKE 'WELLINGTON%' OR upper(description) LIKE 'WAL-MART%'
-					OR upper(description) LIKE 'BATH%' OR upper(description) LIKE 'MCQUADE%' OR upper(description) LIKE 'SHOPPERS%' 
-					OR upper(description) LIKE 'DOLLARAMA%' OR upper(description) LIKE 'HIWAY%'
+	OR upper(description) LIKE 'BATH%' OR upper(description) LIKE 'MCQUADE%' OR upper(description) LIKE 'SHOPPERS%' 
+	OR upper(description) LIKE 'DOLLARAMA%' OR upper(description) LIKE 'HIWAY%'
 GROUP BY MONTH(date)
 )
 SELECT month, total_spent_2021,total_spent_2022, ROUND(100*(total_spent_2022-total_spent_2021)/total_spent_2021,2) as percent_change
@@ -53,8 +53,8 @@ FROM instore_purchase
 SELECT YEAR(date) AS year, SUM(amount) AS total_spent_online_food
 FROM AMEX..Amex
 WHERE upper(description) LIKE 'SKIPTHE%' OR upper(description) LIKE 'DOMINOS%' OR upper(description) LIKE 'TANDOORI%' 
-	  OR upper(description) LIKE 'A&W%' OR upper(description) LIKE 'TIM HORT%' OR upper(description) LIKE 'ZOMATO%' 
-	  OR upper(description) LIKE 'UBER EATS%'
+	OR upper(description) LIKE 'A&W%' OR upper(description) LIKE 'TIM HORT%' OR upper(description) LIKE 'ZOMATO%' 
+	OR upper(description) LIKE 'UBER EATS%'
 GROUP BY YEAR(date)
 
 
@@ -62,12 +62,12 @@ GROUP BY YEAR(date)
 
 WITH online_food as(
 SELECT MONTH(date) AS month, 
-		SUM(CASE WHEN YEAR(date) = 2021 THEN amount END) AS total_spent_2021, 
-		SUM(CASE WHEN YEAR(date) = 2022 THEN amount END) AS total_spent_2022
+	SUM(CASE WHEN YEAR(date) = 2021 THEN amount END) AS total_spent_2021, 
+	SUM(CASE WHEN YEAR(date) = 2022 THEN amount END) AS total_spent_2022
 FROM AMEX..Amex
 WHERE (upper(description) LIKE 'SKIPTHE%' OR upper(description) LIKE 'DOMINOS%' OR upper(description) LIKE 'TANDOORI%' 
-		OR upper(description) LIKE 'A&W%' OR upper(description) LIKE 'TIM HORT%' OR upper(description) LIKE 'ZOMATO%'
-		OR upper(description) LIKE 'UBER EATS%') 
+	OR upper(description) LIKE 'A&W%' OR upper(description) LIKE 'TIM HORT%' OR upper(description) LIKE 'ZOMATO%'
+	OR upper(description) LIKE 'UBER EATS%') 
 GROUP BY MONTH(date)
 )
 SELECT month, total_spent_2021,total_spent_2022, ROUND(100*(total_spent_2022-total_spent_2021)/total_spent_2021,2) as percent_change
@@ -78,11 +78,11 @@ FROM online_food
 
 WITH grocery AS (
 SELECT MONTH(date) AS month, 
-		SUM(CASE WHEN YEAR(date) = 2021 THEN amount END) AS total_spent_2021, 
-		SUM(CASE WHEN YEAR(date) = 2022 THEN amount END) AS total_spent_2022
+	SUM(CASE WHEN YEAR(date) = 2021 THEN amount END) AS total_spent_2021, 
+	SUM(CASE WHEN YEAR(date) = 2022 THEN amount END) AS total_spent_2022
 FROM AMEX..Amex
 WHERE upper(description) LIKE 'FOOD BASIC%' OR upper(description) LIKE 'INDIAN BAZAR%' OR upper(description) LIKE 'INSTA CART%'
-		OR upper(description) LIKE 'METRO%'
+	OR upper(description) LIKE 'METRO%'
 GROUP BY MONTH(date)
 )
 SELECT month, total_spent_2021,total_spent_2022, ROUND(100*(total_spent_2022-total_spent_2021)/total_spent_2021,2) as percent_change
